f409678aa95e2bccd476e794133365e3457e392f
Saqibcoder99 <javaidsaqib853@gmail.com>
2026-08-23 20:37:53 +0500

    UI complete

--- Todo Project/src/App.jsx
+++ Todo Project/src/App.jsx
@@ -8,9 +8,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="min-h-screen max-h-fit flex justify-center items-center bg-[linear-gradient(135deg,#153677,#4e085f)]">
-      <div className="h-auto w-[500px] bg-white p-5 rounded-2xl">
-        <div className="flex justify-center gap-5 mb-6">
+    <div className="min-h-screen max-h-auto flex justify-center items-center bg-[linear-gradient(135deg,#153677,#4e085f)] overflow-auto">
+      <div className="  w-[500px] bg-white p-5 rounded-2xl  ">
+        <div className="flex justify-center gap-5 mb-6 ">
           <h1 className='text-2xl font-bold'>To-Do List</h1>
           <img src="https://cdn-icons-png.flaticon.com/512/4436/4436868.png" alt="" height="32px" width="32px" />
         </div>
@@ -18,7 +18,7 @@ function App() {
           <input type="text" placeholder="Add your todo items...."  className='flex-1 border-none outline-none  p-2.5' />
           <button className='border-none py-4 px-12 bg-[#ff5945] text-white cursor-pointer text-[16px] rounded-[40px]'>Add</button>
         </div>
-        <ul>
+        <ul className='scrollbar-thin overflow-auto h-[auto] py-2.5 min-h-[auto] max-h-[400px] '>
           <li className='list-none flex justify-between bg-[aliceblue] p-3.5 mb-4 rounded-[6px]'>
             Task 1
             <div className="cursor-pointer flex gap-[10px]">
@@ -26,8 +26,9 @@ function App() {
               <i className="fa-regular fa-trash-can text-red-500"></i>           
                </div>
           </li>
+        
         </ul>
-        <div className="flex justify-center items-center"> <button className='text-white bg-[#153677] py-3.5 px-11 text-[16px] cursor-pointer border-none rounded-4xl' >Clear All</button></div>
+        <div className="flex justify-center items-center mt-4"> <button className='text-white bg-[#153677] py-3.5 px-11 text-[16px] cursor-pointer border-none rounded-4xl' >Clear All</button></div>
       </div>
     </div>
 
