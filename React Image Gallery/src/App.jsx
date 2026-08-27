f1aceef474c860789c1e1536c29133169b57cb6b
Saqibcoder99 <javaidsaqib853@gmail.com>
2026-08-27 22:32:43 +0500

    updated

--- React Image Gallery/src/App.jsx
+++ React Image Gallery/src/App.jsx
@@ -5,8 +5,10 @@ import axios from "axios";
 
 function App() {
   const [data, setData] = useState([])
-  const [limit, setlimit] = useState(8)
-  const [page, setpage] = useState(1)
+  const [limit, setLimit] = useState(8)
+  const [page, setPage] = useState(1)
+  const [buttonActive, setButtonActive] = useState(false)
+
 
   const getData=async()=>{
     try{
@@ -26,7 +28,7 @@ function App() {
   return (
     
     <>
-      <nav className='bg-[#fcfcfd] h-[60px] w-[97%] shadow-md flex items-center p-2 rounded-[8px] fixed '>
+      <nav className='bg-[#fcfcfd] h-[60px]  shadow-md flex items-center p-2 rounded-[8px] fixed left-5 right-5  '>
         <div className="flex items-center gap-3">
           <i class="fa-solid fa-image text-[#7763e2] text-4xl"></i>
           <p className='text-[20px] font-semibold'>Gallery</p>
@@ -36,13 +38,13 @@ function App() {
       }
 
       <div className='flex flex-wrap gap-5 justify-around py-7 mt-14'>
-        {data.map((item)=><GalleryCard data={item}/>)}
+       {data.length>0?(data.map((item)=><GalleryCard data={item}/>)):(<p className="text-gray-600  font-bold mt-50 ">Loading...</p>)}
       </div>
 
-      <footer className='bg-[#fcfcfd] h-[60px] shadow-md flex items-center justify-between p-5 rounded-[8px] text-gray-700'>
-        <div className='flex gap-4  items-center'>
+      <footer className='bg-[#fcfcfd] h-[60px] shadow-md flex items-center justify-between  px-2 py-4 rounded-[8px] text-gray-700 mx-5 sm:px-4'>
+        <div className='flex gap-2 text-[12px]  items-center sm:gap-4 sm:text-[18px]'>
           <p>Show</p>
-          <select className='px-2 py-1 border border-[#aaacb5] rounded-[8px] outline-[#919dd6] text-black' onChange={(e)=>setlimit(e.target.value)}  >
+          <select className='px-2 py-1 border border-[#aaacb5] rounded-[8px] outline-[#919dd6] text-black' onChange={(e)=>setLimit(e.target.value)}  >
             <option value="8" >8</option>
             <option value="16">16</option>
             <option value="24">24</option>
@@ -50,11 +52,11 @@ function App() {
             <option value="50">50</option>
           </select>
         </div>
-        <div className='flex gap-5'>
-          {[1,2,3].map((num)=><button className='border border-[#aaacb5] px-3 py-1 rounded-[8px] cursor-pointer' onClick={()=>setpage(num)}>{num}</button>)}
+        <div className='flex gap-2 sm:gap-5'>
+          {[1,2,3].map((num)=><button className={`border border-[#aaacb5] px-3 py-1 rounded-[8px] cursor-pointer ${page===num?'bg-[#7762e2] text-white':''} ` } onClick={()=>(setPage(num)) }>{num}</button>)}
         </div>
-        <div>
-          Page 1 of 3
+        <div className='text-[12px] font-semibold sm:text-[18px] '>
+          Page {page} of 3
         </div>
       </footer>
     </>
